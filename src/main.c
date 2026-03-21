ba5f5735e026598c223b926b7ff93a944c3cfa73
ChloPUPs <alexplaysgamezzz@outlook.com>
2026-03-21 16:27:53 -0700

    move player

--- src/main.c
+++ src/main.c
@@ -9,19 +9,27 @@ int main() {
 
     InitWindow(screenWidth, screenHeight, "First Raylib Game");
 
-    //printf("y: %.2f\n", (float)screenHeight * (2.0f / 3.0f));
     Vector2 playerPos = { (float)screenWidth / 3.0f , (float)screenHeight * (2.0f / 3.0f) };
+    Vector2 playerMovement = { 0.0f, 0.0f };
+    float playerSpeed = 4.0f;
 
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
-        //printf("playerPos: %.2f, %.2f\n", playerPos.x, playerPos.y);
+        // Get Input
+        
+        playerMovement.x = -IsKeyDown(KEY_LEFT) + IsKeyDown(KEY_RIGHT);
+        playerMovement.y = -IsKeyDown(KEY_UP) + IsKeyDown(KEY_DOWN);
 
+        playerPos.x += playerMovement.x * playerSpeed;
+        playerPos.y += playerMovement.y * playerSpeed;
+
+        // vvv
         BeginDrawing();
         
             ClearBackground(RAYWHITE);
 
-            if (IsKeyDown(KEY_UP))
+            if (IsKeyDown(KEY_T))
                 DrawText("First Window!!!!!!", 20, (screenHeight / 2) - 32, 64, BLACK);
             DrawCircleV(playerPos, 20, RED);
 

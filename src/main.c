393de02de0316ee6b06190d790e92801a3565348
ChloPUPs <alexplaysgamezzz@outlook.com>
2026-03-21 16:19:16 -0700

    draw circle

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <stdio.h>
 
 #include "../include/raylib.h"
 
@@ -8,15 +9,21 @@ int main() {
 
     InitWindow(screenWidth, screenHeight, "First Raylib Game");
 
+    //printf("y: %.2f\n", (float)screenHeight * (2.0f / 3.0f));
+    Vector2 playerPos = { (float)screenWidth / 3.0f , (float)screenHeight * (2.0f / 3.0f) };
+
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
+        //printf("playerPos: %.2f, %.2f\n", playerPos.x, playerPos.y);
+
         BeginDrawing();
         
             ClearBackground(RAYWHITE);
 
             if (IsKeyDown(KEY_UP))
                 DrawText("First Window!!!!!!", 20, (screenHeight / 2) - 32, 64, BLACK);
+            DrawCircleV(playerPos, 20, RED);
 
         EndDrawing();
     }
